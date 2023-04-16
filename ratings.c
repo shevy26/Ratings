c952e6b8dab1a6bc63c4cfc75367d6d1f73a8227
shevy26 <127989608+shevy26@users.noreply.github.com>
2023-04-16 21:23:33 +0530

    Create ratings.c

---
+++ ratings.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+// in this Y represents your ratings
+void main()
+{
+    char input[1]="Y";
+    int ratings;
+    printf("Enter your ratings (1-5) :\n");
+    scanf("%d",&ratings);
+    if(ratings<=5&& ratings>=1)
+{
+       for(int i=1;i<=5;i++)
+     {
+          for(int j=1;j<=i;j++)
+        {
+            if(ratings==i)
+            {
+             printf(" %s ",input);    
+            }
+            else{
+                printf(" * ");
+            }
+        }
+          printf("\n");
+    }
+}
+else{
+    printf("*=*=*=*= Invalid Ratings =*=*=*=*\n");
+}
+}
